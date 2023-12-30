00534a48a06d4125a0fc2a9f2e104ff30917c3a3
amalendu anil <AmalenduA>
2023-12-30 09:44:38 +0530

    first commit

---
+++ layout/header.php
@@ -0,0 +1,41 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ANTIQUES</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home decor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#" tabindex="-1" >Garden decor</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#" tabindex="-1" >About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="#" tabindex="-1" >Contact us</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <button type="button" class="btn btn-light"><a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+            </svg></a></button>
+
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+       
+        <button type="button" class="btn btn-light"> <a  href="/decor/template/login.php">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-add" viewBox="0 0 16 16">
+            <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+            <path d="M2 13c0 1 1 1 1 1h5.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.544-3.393C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4"/></svg></a>
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ template/login.php
@@ -0,0 +1,91 @@
+<?php
+$title = "Login";
+?>
+<?php include_once("../layout/head.php") ?>
+<?php include_once("../layout/header.php") ?>
+
+<div class="container shadow rounded mt-10 p-15" >
+<!-- Section: Design Block -->
+    <section class="text-center text-lg-start">
+        <style>
+            .cascading-right {
+              margin-right: -50px;
+            }
+
+            @media (max-width: 991.98px) {
+              .cascading-right {
+                margin-right: 0;
+              }
+            }
+        </style>
+
+  <!-- Jumbotron -->
+        <div class="container py-4">
+          <div class="row g-0 align-items-center">
+            <div class="col-lg-6 mb-5 mb-lg-0">
+                <div class="card cascading-right" style="
+                    background: hsla(0, 0%, 100%, 0.55);
+                    backdrop-filter: blur(30px);
+                    ">
+                <div class="card-body p-5 shadow-5 text-center">
+                  <h2 class="fw-bold mb-5">Login </h2>
+                  <form action="#" method="post">
+                    <!-- Email input -->
+                    <div class="form-outline mb-4">
+                    <input type="email" id="form3Example3" class="form-control" name="email">
+                    <label class="form-label" for="form3Example3">Email address</label>
+                  </div>
+    
+                  <!-- Password input -->
+                  <div class="form-outline mb-4">
+                    <input type="password" id="form3Example4" class="form-control" name="password">
+                    <label class="form-label" for="form3Example4">Password</label>
+                  </div>
+    
+              <!-- Checkbox -->
+              <div class="form-check d-flex justify-content-center mb-4">
+                <input class="form-check-input me-2" type="checkbox" value="" id="form2Example33" checked />
+                <label class="form-check-label" for="form2Example33">
+                  agree terms and conditions
+                </label>
+              </div>
+
+              <!-- Submit button -->
+              <button type="submit" class="btn btn-success btn-block mb-4">
+                Sign up
+              </button>
+
+              <!-- Register buttons -->
+              <p>Don't have an account? <a href="/decor/template/registration.php" class="link-info">Register here</a></p>
+
+            </form>
+          </div>
+        </div>
+      </div>
+
+            <div class="col-lg-6 mb-5 mb-lg-0">
+              <img src="../images/image4.jpg" class="w-50 rounded-4 shadow-4" alt="...">
+            </div>
+        </div>
+        <div>
+  <!-- Jumbotron -->
+    </section>
+<!-- Section: Design Block -->
+
+<br><br><br>
+<?php include_once("../layout/footer.php") ?>
+
+<?php 
+  // Request
+  if ($_SERVER['REQUEST_METHOD']=="POST") {
+    // Add user model
+      include('../model/user.php');
+      $user_model=new User();
+
+      $email=$_REQUEST['email'];
+      $password=$_REQUEST['password'];
+
+      // insert
+      $user_model->login($email,$password);
+  }
+?>
